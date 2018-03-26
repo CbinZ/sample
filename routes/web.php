76296b02037ea127a6ffb45769b88a49eaaa709b
cbin <2309970475@qq.com>
2018-03-26 08:59:34 +0000

    Finish user signup

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 //用户注册
 
 Route::get('signup','UsersController@create')->name('signup');
+
+//用户资源
+Route::resource('users','UsersController');
