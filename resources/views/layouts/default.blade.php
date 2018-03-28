d72fc72b189e22cafee2d52466f5e53d4f7bc0e0
cbin <2309970475@qq.com>
2018-03-28 03:38:29 +0000

    Finish login and logout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,20 +5,6 @@
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-   {{-- <header class="navbar navbar-fixed-top navbar-inverse">
-        <div class="container">
-            <div class="col-md-offset-1 col-md-10">
-                <a href="/" id="logo">Sample App</a>
-                <nav>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="/help">帮助</a></li>
-                        <li><a href="#">登录</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </header>--}}
-
    @include('layouts._header')
 
         <div class="container">
@@ -29,5 +15,7 @@
                 @include('layouts._footer')
             </div>
         </div>
+
+     <script src="/js/app.js"></script>
     </body>
 </html>
